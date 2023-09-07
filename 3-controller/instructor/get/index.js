3fbf4282deb6a4f086dbb6ca79f846ec6e5a872c
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2023-09-07 22:23:12 +0800

    finish routes

--- 3-controller/instructor/get/index.js
+++ 3-controller/instructor/get/index.js
@@ -2,19 +2,47 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-// Define a function to handle the "/hello" route
-function helloWorldController(req, res) {
-  // Set the content type to HTML
-  res.setHeader("Content-Type", "text/html");
-
-  // Send the HTML response
-  res.send(
-    "<!DOCTYPE html><html><head><title>Hello World</title></head><body><h1>Hello, World!</h1></body></html>"
-  );
-}
+// //Define a function to handle the "/hello" route
+// function helloWorldController(req, res) {
+//   console.log(req.params);
+//   // Set the content type to HTML
+//   res.setHeader("Content-Type", "text/html");
+//   // Send the HTML response
+//   res.send(
+//     "<!DOCTYPE html><html><head><title>Hello World</title></head><body><h1>Hello, World!</h1></body></html>"
+//   );
+// }
 
 // Define a route for the "/hello" path and associate it with the controller
-app.get("/hello", helloWorldController);
+
+app.get("/user/admin", (req, res) => {
+  // Set the content type to HTML
+  res.setHeader("Content-Type", "text/html");
+  // Send the HTML response
+  res.send(
+    `<!DOCTYPE html><html><head><title>Hello World</title></head><body><h1>Welcome to admin dashboard!</h1></body></html>`
+  );
+});
+
+app.get("/user/:username", (req, res) => {
+  const username = req.params.username;
+  // Set the content type to HTML
+  res.setHeader("Content-Type", "text/html");
+  // Send the HTML response
+  res.send(
+    `<!DOCTYPE html><html><head><title>Hello World</title></head><body><h1>Hello, ${username}!</h1></body></html>`
+  );
+});
+
+// 404 routes
+app.use((req, res) => {
+  // Set the content type to HTML
+  res.setHeader("Content-Type", "text/html");
+  // Send the HTML response
+  res.send(
+    `<!DOCTYPE html><html><head><title>Hello World</title></head><body><h1>404 Not Found!</h1></body></html>`
+  );
+});
 
 // Start the server
 app.listen(port, () => {
