aa5a8583bab9de7ff42501a375a8fcd3f1798ff2
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2023-09-21 23:05:52 +0800

    finish

--- 6-rest-api/intructor/introduction/middleware/isAuthenticated.js
+++ 6-rest-api/intructor/introduction/middleware/isAuthenticated.js
@@ -4,12 +4,13 @@ const isAuthenticated = (req, res, next) => {
     const token = req.headers.authorization.split(" ")[1];
     const decoded = jwt.verify(token, "SECRET-KEY-I-PUT-MYSELF");
     // IMPORTANT!!!!!
+    // early guard clause, everything error or negative
     if (!decoded?.id) return res.status(401).json({ message: "Unauthorised" });
     // IMPORTANT!!!!!
     req.user = decoded.id;
     next();
   } catch (error) {
-    res.status(401).json({ message: "Unauthorised" });
+    res.status(401).json({ message: "Unauthorised", error });
   }
 };
 
