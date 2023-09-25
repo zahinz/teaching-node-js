ecf26dbfbd5ad71899ed11ff619a9ceacd23df21
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2023-09-25 22:53:45 +0800

    wip

--- 6-rest-api/intructor/introduction/index.js
+++ 6-rest-api/intructor/introduction/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import apiRoutes from "./routes/index.js";
 import { dbInit } from "./database/connection.js";
 
@@ -7,6 +8,7 @@ const PORT = 8080;
 
 dbInit();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
