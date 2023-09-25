1b85c352bf1c2ff2c828d27cd15d28e282cc6cd9
Zahin Zulkipli <64232111+zahinz@users.noreply.github.com>
2023-09-25 22:53:27 +0800

    wip

--- 7-react-integration/instructor/react-express/src/pages/Login.jsx
+++ 7-react-integration/instructor/react-express/src/pages/Login.jsx
@@ -2,8 +2,37 @@ import React from "react";
 import Page from "../components/Page";
 import Header from "../components/Header";
 import Container from "../components/Container";
+import axios from "axios";
+import Cookies from "universal-cookie";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const cookies = new Cookies(null, { path: "/" });
+  const navigate = useNavigate();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // get all values from form
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+    const reqBody = { email: email, password: password };
+
+    // send request to server
+    try {
+      // success scope
+      const res = await axios.post("http://localhost:8080/login", reqBody);
+      console.log(res);
+      alert("Login successful!");
+      // store jwt token into cookie
+      cookies.set("token", res.data.token);
+      // redirect to admin page
+      navigate("/admin");
+    } catch (error) {
+      // error scope
+      console.error(error);
+      alert(error.response.data.message);
+    }
+  };
+
   return (
     <Page>
       <Header />
@@ -14,7 +43,7 @@ const Login = () => {
           alignItems: "center",
         }}
       >
-        <div style={formContainer}>
+        <form style={formContainer} onSubmit={(e) => handleSubmit(e)}>
           <h5>Login</h5>
           <div style={inputContainer}>
             <label htmlFor="email">Email</label>
@@ -24,11 +53,11 @@ const Login = () => {
             <label htmlFor="password">Password</label>
             <input type="text" name="password" id="password" />
           </div>
-          <button type="button">Login</button>
+          <button type="submit">Login</button>
           <button type="button" className="btn-secondary">
             Register as new user
           </button>
-        </div>
+        </form>
       </Container>
     </Page>
   );
--- 7-react-integration/instructor/react-express/src/pages/Register.jsx
+++ 7-react-integration/instructor/react-express/src/pages/Register.jsx
@@ -2,8 +2,35 @@ import React from "react";
 import Page from "../components/Page";
 import Header from "../components/Header";
 import Container from "../components/Container";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // get all values from form
+    const email = e.target[0].value;
+    const username = e.target[1].value;
+    const password = e.target[2].value;
+    const reqBody = { email: email, username: username, password: password };
+
+    // send request to server
+    try {
+      // success scope
+      const res = await axios.post("http://localhost:8080/register", reqBody);
+      console.log(res);
+      alert("Registration successful!");
+      // redirect to login page
+      navigate("/login");
+    } catch (error) {
+      // error scope
+      console.error(error);
+      alert(error.response.data.message);
+    }
+  };
+
   return (
     <Page>
       <Header />
@@ -14,25 +41,28 @@ const Register = () => {
           alignItems: "center",
         }}
       >
-        <div style={formContainer}>
+        <form style={formContainer} onSubmit={(event) => handleSubmit(event)}>
           <h5>Register</h5>
+          {/* email */}
           <div style={inputContainer}>
             <label htmlFor="email">Email</label>
             <input type="text" name="email" id="email" />
           </div>
+          {/* username */}
           <div style={inputContainer}>
             <label htmlFor="username">Username</label>
             <input type="text" name="username" id="username" />
           </div>
+          {/* password */}
           <div style={inputContainer}>
             <label htmlFor="password">Password</label>
             <input type="text" name="password" id="password" />
           </div>
-          <button type="button">Register</button>
+          <button type="submit">Register</button>
           <button type="button" className="btn-secondary">
             Login as existing user
           </button>
-        </div>
+        </form>
       </Container>
     </Page>
   );
